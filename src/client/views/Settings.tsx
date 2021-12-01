d3ae8c262c894012381d75a2be90ed70c0525d47
Tommaso Morganti <tommaso.morganti01@gmail.com>
2021-12-01 14:41:47 +0100

    settings layout polishing

--- src/client/views/Settings.tsx
+++ src/client/views/Settings.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useEffect, useState } from 'react'
 import _Switch from 'react-switch'
-import { IoWarning } from 'react-icons/io5'
-import { ipcRenderer } from 'electron'
+import { IoClose, IoWarning } from 'react-icons/io5'
+import { ipcRenderer, shell } from 'electron'
 import { Modal } from '../components/Modal'
 
 import { Settings } from '../../helpers/store'
@@ -27,7 +27,17 @@ export let Switch: FC<{
         height={22}
         width={40}
         checked={props.checked}
+        className="switch"
     />
+}
+
+let Link: FC<{ href: string }> = props => {
+    return <a onClick={e => {
+        e.preventDefault()
+        shell.openExternal(props.href)
+    }}>
+        {props.children}
+    </a>
 }
 
 let prevTheme: Theme // the previous theme is stored in case the users cancel the settigns change
@@ -47,65 +57,87 @@ export let SettingsModal: FC<{ onClose: () => void }> = (props) => {
     return <Modal onClose={() => props.onClose()}>
         {settings ? <div className="settings">
             <h3>Settings</h3>
-            {theme ? <div className="setting">
-                <span>Color theme</span>
-                <select value={theme} onChange={e => {
-                    let theme = e.target.value as Theme
-                    ipcRenderer.send('set-native-theme', theme)
-                    setTheme(theme)
-                }}>
-                    <option value="system">use system</option>
-                    <option value="light">light theme</option>
-                    <option value="dark">dark theme</option>
-                </select>
-            </div> : undefined}
-            <div className="setting">
-                <span>Keep the up running in background when all windows are closed</span>
-                <Switch
-                    onChange={v => updateSettigns({ ...settings, keepOpenInBackground: v })}
-                    checked={settings.keepOpenInBackground}
-                />
+            <IoClose className="close clickable" onClick={() => { props.onClose() }} />
+            <div className="setting-section">
+                {theme ? <div className="setting">
+                    <span>Color theme</span>
+                    <select value={theme} onChange={e => {
+                        let theme = e.target.value as Theme
+                        ipcRenderer.send('set-native-theme', theme)
+                        setTheme(theme)
+                    }}>
+                        <option value="system">use system</option>
+                        <option value="light">light theme</option>
+                        <option value="dark">dark theme</option>
+                    </select>
+                </div> : undefined}
+                <div className="setting">
+                    <span>Language</span>
+                    <select>
+                        <option value="it">Italiano</option>
+                        <option value="en">English</option>
+                    </select>
+                </div>
             </div>
-            <div className={`setting ${settings.keepOpenInBackground ? '' : 'disabled'}`}>
-                <span>Show app in tray</span>
-                <Switch
-                    disabled={!settings.keepOpenInBackground}
-                    onChange={v => updateSettigns({ ...settings, trayIcon: v })}
-                    checked={settings.keepOpenInBackground && settings.trayIcon}
-                />
+            <div className="setting-section">
+                <div className="setting">
+                    <span>Open app at login</span>
+                    <Switch
+                        onChange={v => updateSettigns({ ...settings, openAtLogin: v })}
+                        checked={settings.openAtLogin}
+                    />
+                </div>
+                <div className="setting">
+                    <span>Keep open in background</span>
+                    <Switch
+                        onChange={v => updateSettigns({ ...settings, keepOpenInBackground: v })}
+                        checked={settings.keepOpenInBackground}
+                    />
+                    <span className="desc">
+                        keep the app running in background while all windows are closed, if autosync is
+                        enabled, they will happen in background too
+                    </span>
+                </div>
+                <div className={`setting ${settings.keepOpenInBackground ? '' : 'disabled'}`}>
+                    <span>Show app in tray</span>
+                    <Switch
+                        disabled={!settings.keepOpenInBackground}
+                        onChange={v => updateSettigns({ ...settings, trayIcon: v })}
+                        checked={settings.keepOpenInBackground && settings.trayIcon}
+                    />
+                </div>
+                {settings.keepOpenInBackground && !settings.trayIcon ? <div className="setting-warn">
+                    <IoWarning />
+                    <span>
+                        With the tray disabled, you wont be able to tell if the app is running.
+                        Once you close the window, you'll need to relaunch the app to open it again.
+                        If you have autosyncs enabled, those will keep going in the background
+                    </span>
+                </div> : undefined}
             </div>
-            {settings.keepOpenInBackground && !settings.trayIcon ? <div className="setting-warn">
-                <IoWarning />
-                <span>
-                    With the tray disabled, you wont be able to tell if the app is running.
-                    Once you close the window, you'll need to relaunch the app to open it again.
-                    If you have autosyncs enabled, those will keep going in the background
-                </span>
-            </div> : undefined}
-            <div className="setting">
-                <span>Open app at login</span>
-                <Switch
-                    onChange={v => updateSettigns({ ...settings, openAtLogin: v })}
-                    checked={settings.openAtLogin}
-                />
-            </div>
-            <div className="setting">
-                <span>Sync new courses when they are found</span>
-                <Switch
-                    onChange={v => updateSettigns({ ...settings, syncNewCourses: v })}
-                    checked={settings.syncNewCourses}
-                />
+            <div className="setting-section">
+                <div className="setting">
+                    <span>Sync new courses</span>
+                    <Switch
+                        onChange={v => updateSettigns({ ...settings, syncNewCourses: v })}
+                        checked={settings.syncNewCourses}
+                    />
+                    <span className="desc">
+                        When new courses are found, automatically add them to the courses that will
+                        be synced
+                    </span>
+                </div>
             </div>
             <button className="danger-button" onClick={() => {
                 ipcRenderer.send('logout')
             }}>log out</button>
 
-            {/* <span className="credits">
-                Developed by Tommaso Morganti • <a href="https://github.com/toto04">GitHub</a>
+            <span className="credits">
+                Developed by Tommaso Morganti • <Link href="https://github.com/toto04">GitHub</Link>
                 <br />
-                <a href="https://github.com/toto04/webeep-sync">Source code</a> •
-                <a href="https://github.com/toto04/webeep-sync/issues"> Report a Bug</a>
-            </span> */}
+                <Link href="https://github.com/toto04/webeep-sync">Source code</Link> •&nbsp;
+                <Link href="https://github.com/toto04/webeep-sync/issues">Report a Bug</Link>
+            </span>
 
             <div className="button-line-container">
                 <button className="discard-button" onClick={() => {
