80f789592bb07e1ea0881ad10e7632dcb7910707
Tommaso Morganti <tommaso.morganti01@gmail.com>
2021-12-26 21:24:59 +0100

    new files modal translation

--- src/client/views/NewFilesModal.tsx
+++ src/client/views/NewFilesModal.tsx
@@ -6,9 +6,12 @@ import { Modal } from '../components/Modal'
 import { formatSize } from '../../util'
 import { NewFilesList } from '../../helpers/download'
 import { shell } from 'electron'
+import { useTranslation } from 'react-i18next'
 
 let NewFilesCourseCollapsable: FC<{ name: string, files: NewFilesList[string] }> = props => {
     let [isOpen, toggle] = useState(true)
+    let { t } = useTranslation('client', { keyPrefix: 'newFiles' })
+
     return <div className="course-collapsable">
         <h4 onClick={() => toggle(!isOpen)}>
             {isOpen ? <IoCaretDown /> : <IoCaretForward />} {props.name} ({props.files.length})
@@ -29,13 +32,13 @@ let NewFilesCourseCollapsable: FC<{ name: string, files: NewFilesList[string] }>
                         shell.showItemInFolder(file.absolutePath)
                     }}>
                         <IoFolderOpen />
-                        <span>reveal</span>
+                        <span>{t(`reveal`)}</span>
                     </div>
                     <div className="clickable" onClick={() => {
                         shell.openPath(file.absolutePath)
                     }}>
                         <IoOpen />
-                        <span>open</span>
+                        <span>{t(`open`)}</span>
                     </div>
                 </div>)
             : undefined}
@@ -43,11 +46,13 @@ let NewFilesCourseCollapsable: FC<{ name: string, files: NewFilesList[string] }>
 }
 
 export let NewFilesModal: FC<{ onClose: () => void, files: NewFilesList }> = (props) => {
-    let numfiles = 0
+    let { t } = useTranslation('client', { keyPrefix: 'newFiles' })
+
+    let count = 0
     for (let course in props.files) {
-        numfiles += props.files[course].length
+        count += props.files[course].length
     }
-    return <Modal title={`${numfiles} new files downloaded`} onClose={() => props.onClose()}>
+    return <Modal title={t(`newFiles`, { count })} onClose={() => props.onClose()}>
         <div className="new-files-modal">
             {Object.entries(props.files).map(([course, files]) => <NewFilesCourseCollapsable
                 key={'collaps' + course}
