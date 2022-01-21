08e96d37e871624930699897136442f6ab373c8f
Tommaso Morganti <tommaso.morganti01@gmail.com>
2022-01-22 00:41:09 +0100

    update manager file

---
+++ src/modules/updates.ts
@@ -0,0 +1,58 @@
+import { EventEmitter } from 'events'
+
+import { app } from 'electron'
+import got from 'got'
+import { createLogger } from './logger'
+const { debug, log } = createLogger('updater')
+
+class UpdateManager extends EventEmitter {
+    /**
+     * string containing the latest version, null if no newer version is available
+     */
+    availableUpdate: string | null
+
+    /**
+     * this function check via the github api what is the latest release and confronts it with 
+     * @returns null if there is no new version, otherwise a string containing the new version (e.g 0.5.1)
+     */
+    private async getLatestUpdate(): Promise<string | null> {
+        let versionRE = /(\d+)\.(\d+)\.(\d+)/
+        try {
+            debug('checking for new version')
+            let res = await got.get('https://api.github.com/repos/toto04/webeep-sync/releases/latest', {
+                headers: {
+                    "Accept": 'application/vnd.github.v3+json'
+                }
+            })
+            let latestVersion: string = JSON.parse(res.body).tag_name.substring(1)
+            let latestMatch = latestVersion.match(versionRE)
+            let oldMatch = app.getVersion().match(versionRE)
+            latestMatch.splice(0, 1)
+            oldMatch.splice(0, 1)
+
+            let latestV: number[] = latestMatch.map(v => parseInt(v))
+            let oldV: number[] = oldMatch.map(v => parseInt(v))
+
+            console.log({ latestVersion, latestMatch, latestV, oldMatch, oldV })
+
+            let newVersion = latestV[0] > oldV[0] || latestV[1] > oldV[1] || latestV[2] > oldV[2]
+            log(`Found new version v${latestVersion}!`)
+            if (newVersion) return latestVersion
+        } catch (e) {
+            debug('could not check for new version')
+        }
+        return null
+    }
+
+    /**
+     * checks if a new update is available, and caches it in the {@link availableUpdate} string
+     * @returns null if there is no new versio, otherwise a string containing the new version (e.g 0.5.1)
+     */
+    async checkUpdate() {
+        let update = await this.getLatestUpdate()
+        this.availableUpdate = update
+        return update
+    }
+}
+
+export let updates = new UpdateManager()
