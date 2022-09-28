dac3f29bff318862f40799764e08f9f2a5a357b9
Tommaso Morganti <tommaso.morganti01@gmail.com>
2022-09-28 15:28:23 +0200

    modified to .cer

--- macos_cert.sh
+++ macos_cert.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 KEY_CHAIN=build.keychain
-CERTIFICATE_P12=certificate.p12
+CERTIFICATE=certificate.cer
 
 # Recreate the certificate from the secure environment variable
-echo $MACOS_CERT_BASE64 | base64 --decode > $CERTIFICATE_P12
+echo $MACOS_CERT_BASE64 | base64 --decode > $CERTIFICATE
 
 #create a keychain
 security create-keychain -p actions $KEY_CHAIN
@@ -15,9 +15,9 @@ security default-keychain -s $KEY_CHAIN
 # Unlock the keychain
 security unlock-keychain -p actions $KEY_CHAIN
 
-security import $CERTIFICATE_P12 -k $KEY_CHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign;
+security import $CERTIFICATE -k $KEY_CHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign;
 
 security set-key-partition-list -S apple-tool:,apple: -s -k actions $KEY_CHAIN
 
 # remove certs
-rm -fr *.p12
+rm -fr *.cer
